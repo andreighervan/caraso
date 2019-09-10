1271ef488a411e729b044ee4f0fbd8f11ad58cb1
andreighervan <andrei.ghervan8@gmail.com>
2019-09-10 21:17:05 +0300

    add posts component html and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PopularCarsComponent } from './home/popular-cars/popular-cars.component';
-import { SliderComponent } from './home/slider/slider.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    PopularCarsComponent,
-    SliderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
