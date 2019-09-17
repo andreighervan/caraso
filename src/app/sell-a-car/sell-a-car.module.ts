ce5ba66fa8fa851482ca2222bf01f67d65e15077
andreighervan <andrei.ghervan8@gmail.com>
2019-09-17 19:15:34 +0300

    add breadcrumb component for sell-a-car page html scss

--- src/app/sell-a-car/sell-a-car.module.ts
+++ src/app/sell-a-car/sell-a-car.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SellACarRoutingModule } from './sell-a-car-routing.module';
 import { SellACarComponent } from './sell-a-car.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 
 @NgModule({
-  declarations: [SellACarComponent],
+  declarations: [SellACarComponent, BreadcrumbComponent],
   imports: [
     CommonModule,
     SellACarRoutingModule
