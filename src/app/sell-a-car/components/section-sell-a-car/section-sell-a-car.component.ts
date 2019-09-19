4ff45ae68c737616ae51d62aa8d84b274e9a13ca
andreighervan <andrei.ghervan8@gmail.com>
2019-09-19 20:14:40 +0300

    add for html css and ts for sell a car component

--- src/app/sell-a-car/components/section-sell-a-car/section-sell-a-car.component.ts
+++ src/app/sell-a-car/components/section-sell-a-car/section-sell-a-car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
