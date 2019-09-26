65c41b4a07d38adce36d261c39aedcca640843f1
andreighervan <andrei.ghervan8@gmail.com>
2019-09-26 19:38:22 +0300

    add sidebar for inventory html and scss

--- src/app/inventory/inventory-sidebar/inventory-sidebar.component.ts
+++ src/app/inventory/inventory-sidebar/inventory-sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-inventory-sidebar',
@@ -9,7 +10,5 @@ export class InventorySidebarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
