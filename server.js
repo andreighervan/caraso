f64ceeb2504b2836e2ca505782e54bc38a8b5af8
andreighervan <andrei.ghervan8@gmail.com>
2019-09-27 20:14:19 +0300

    update server.js add car model

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ let express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
+    Car = require('./api/models/carModel'), //created model loading here
     bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://andreighervan:123QQsuccess@ds017165.mlab.com:17165/caraso', { useNewUrlParser: true });
+mongoose.connect('mongodb://andreighervan:123QQsuccess@ds119368.mlab.com:19368/caraso', { useUnifiedTopology: true, useNewUrlParser: true } );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
